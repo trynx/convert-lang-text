94ff612838a428aa14b434da64d4f549609a3dd8
nicodo93 <nicolas@spinomenal.com>
2022-05-01 10:52:52 +0300

    Add change language

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 
 import time
-from ctypes import Structure, byref, c_long, windll
+# from ctypes import Structure, byref, c_long, windll
 import tkinter as tk
-import pyautogui as pya
+# import pyautogui as pya
+import win32api
 import keyboard
-
 
 
 def main(argv=None):
@@ -14,8 +14,6 @@ def main(argv=None):
     save_selected_text()
     keyboard.wait()
 
-    # TODO: Get next language (alt+shift)
-    # TODO: Get the original text and change each word by the place in the keyboard to be the new language
     # TODO: Return the new text to the selected area (paste?)
     # TODO: Return the original clipboard
 
@@ -25,23 +23,42 @@ def save_selected_text():
 
 
 def save_text():
-    # After some experiments, need to wait for arouns 200ms~
-    time.sleep(0.2)
+    wait_before_keyboard()
     keyboard.send('ctrl+c')
-    # pya.hotkey('ctrl', 'c')
-    # time.sleep(0.1)
+
     print(get_clipboard_text())
-    # time.sleep(0.1)
 
-
+    change_lang()
 
 # Get clipboard data
+
+
 def get_clipboard_text():
     """ https://stackoverflow.com/questions/101128/how-do-i-read-text-from-the-clipboard/49646482#49646482 """
     root = tk.Tk()
     # keep the window from showing
     root.withdraw()
     return root.clipboard_get()
+
+
+def change_lang():
+    # Get next language (alt+shift)
+    wait_before_keyboard()
+    keyboard.send('alt+shift')
+    
+    reformat_text()
+
+
+def reformat_text():
+    # TODO: Get the original text and change each word by the place in the keyboard to be the new language
+    originalText = get_clipboard_text()
+
+    print('Before', originalText)
+
+
+def wait_before_keyboard():
+    # After some experiments, need to wait for arouns 200ms~
+    time.sleep(0.2)
 
 
 if __name__ == '__main__':
